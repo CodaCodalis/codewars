f6bd8af1eb861c8dec7cf7832fdda3642cd42558
CodaCodalis <larah.wuxus@gmail.com>
2020-10-19 12:35:59 +0200

    Prime-prober added

---
+++ src/Prime.java
@@ -0,0 +1,25 @@
+public class Prime {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        } else if (num == 2) {
+            return true;
+        } else if (num % 2 == 0) {
+            return false;
+        } else {
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(3));
+        System.out.println(isPrime(4));
+    }
+}
