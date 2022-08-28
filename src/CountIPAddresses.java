91cbddf956924b10a8e0c2c7fb2fc6c3a8e29127
CodaCodalis <lara.wuxus@gmail.com>
2022-08-28 12:43:35 +0200

    convert decimal ip to binary ip completed

--- src/CountIPAddresses.java
+++ src/CountIPAddresses.java
@@ -6,8 +6,8 @@ public class CountIPAddresses {
         int[] arrOne = splitBySeparator(one);
         int[] arrTwo = splitBySeparator(two);
         
-        String ipOneAsBinary = convertToBinary(arrOne);
-        String ipTwoAsBinary = convertToBinary(arrTwo);
+        String[] ipOneAsBinary = convertToBinary(arrOne);
+        String[] ipTwoAsBinary = convertToBinary(arrTwo);
         
         //String resultAsBinary = subtractBinaries(ipTwoAsBinary, ipOneAsBinary);
 
@@ -20,36 +20,68 @@ public class CountIPAddresses {
         return decimal;
     }
 
-    private static String subtractBinaries(String ipTwoAsBinary, String ipOneAsBinary) {
+    private static String subtractBinaries(String[] ipTwoAsBinaryArr, String[] ipOneAsBinaryArr) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < ipOneAsBinaryArr.length; i++) {
+            sb1.append(ipOneAsBinaryArr[i]);
+        }
+        String ipOneAsBinary = sb1.toString();
+        for (int i = 0; i < ipOneAsBinaryArr.length; i++) {
+            sb2.append(ipOneAsBinaryArr[i]);
+        }
+        String ipTwoAsBinary = sb2.toString();
         int one = Integer.parseInt(ipOneAsBinary);
         int two = Integer.parseInt(ipTwoAsBinary);
         int sum = one + (~two + 1);
         return Integer.toBinaryString(sum);
     }
 
-    private static String convertToBinary(int[] intArr) {
-        StringBuilder sb = new StringBuilder("");
+    private static String[] convertToBinary(int[] intArr) {
+        String[] binaryIPStrArr = new String[4];
+        String[] binaryOctStrArr = new String[8];
+        String binaryOctStr = "";
         for (int i = 0; i <= 3; i++) {
-            int[] binary = new int[8];
+            System.out.println("Dezimal: " + intArr[i]);
+            binaryOctStr = "";
             int index = 0;
+            boolean marker = false;
             while (intArr[i] > 0) {
-                binary[index++] = intArr[i] % 2;
+                binaryOctStrArr[index] = String.valueOf(intArr[i] % 2);
+                index++;
                 intArr[i] = intArr[i] / 2;
+                if (intArr[i] == 0 && index < 7) {
+                    marker = true;
+                    StringBuilder tooShortOcts = new StringBuilder();
+                    String string = "";
+                    index = index - 1;
+                    for (int k = index; k >= 0; k--) {
+                        tooShortOcts.append(binaryOctStrArr[k]);
+                    }
+                    string = tooShortOcts.toString();
+                    while (index <= 7) {
+                        string = "0" + string;
+                        index++;
+                    }
+                    index = index + 1;
+                    binaryIPStrArr[i] = string;
+                    //System.out.println("Binary: " + i + ". Oktett: " + string+ "");
+                }
             }
-            for (int j = index-1; j >= 0; j--) {
-                sb.append(binary[j]);
+            if (!marker) {
+                for (int j = index - 1; j >= 0; j--) {
+                    binaryOctStr = binaryOctStr + binaryOctStrArr[j];
+                }
+                binaryIPStrArr[i] = binaryOctStr;
+                //System.out.println("Binary: " + i + ". Oktett: " + binaryOctStr + "");
             }
-            /*
-            String string = "";
-            sb.toString();
-            while (string.length() <= 8) {
-                string = "0" + sb;
-            }
-             */
-            System.out.println("Length: " + sb.toString().length());
-            System.out.println("Binary: " + sb);
         }
-        return sb.toString();
+        System.out.print("IP as binary: ");
+        for (int i = 0; i < binaryIPStrArr.length; i++) {
+            System.out.print(binaryIPStrArr[i] + " ");
+        }
+        System.out.println();
+        return binaryIPStrArr;
     }
 
     private static int[] splitBySeparator(String string) {
