680449aab1cabd25aebeafc46a8f50ef6259db7f
CodaCodalis <lara.wuxus@gmail.com>
2022-09-05 23:32:33 +0200

    :/

--- java/java/src/main/java/User.java
+++ java/java/src/main/java/User.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ranges.RangeException;
+
 public class User {
     int rank = -8;
     int progress = 0;
@@ -6,18 +8,31 @@ public class User {
     }
 
     public int incProgress(int activityRank) {
-        if (this.rank < 0 && activityRank < 0) {
-            int userRank = Math.abs(this.rank);
-            activityRank = Math.abs(activityRank);
-            int diff = userRank - activityRank;
+        if (activityRank > 8 || activityRank < -8  || activityRank == 0) {
+            throw new RangeException((short) 1,"out of range");
+        }
+
+        int diff = 0;
+        if (activityRank < 0 && this.rank < 0) {
+            diff = Math.abs(activityRank) + this.rank;
+        } else if (activityRank >= 0 && this.rank < 0) {
+            diff = activityRank + Math.abs(this.rank);
+        } else if (activityRank >= 0 && this.rank > 0) {
+            diff = this.rank - activityRank;
+        }
+        if (this.rank == activityRank) {
+            this.progress += 3;
+        } else if (this.rank == activityRank + 1) {
+            this.progress += 1;
+        } else {
             this.progress += ((int) Math.pow(diff, 2)) * 10;
         }
-        if (this.progress >= 100) {
+        while (this.progress >= 100) {
             this.progress -= 100;
             this.rank += 1;
-        }
-        if (this.rank == 0) {
-            this.rank += 1;
+            if (this.rank == 0) {
+                this.rank += 1;
+            }
         }
         return this.progress;
     }
--- java/java/src/test/java/UserTest.java
+++ java/java/src/test/java/UserTest.java
@@ -1,11 +1,13 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.ranges.RangeException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
     @Test
-    void incProgress() {
+    void negActRankNegRank() {
         User pete = new User();
 
         assertEquals(-8, pete.rank);
@@ -17,4 +19,83 @@ class UserTest {
         assertEquals(-7, pete.rank);
         assertEquals(0, pete.progress);
     }
+    @Test
+    void posActRankNegRank() {
+        User peter = new User();
+
+        assertEquals(-8, peter.rank);
+        assertEquals(0, peter.progress);
+        assertEquals(10, peter.incProgress(1));
+        assertEquals(1, peter.rank);
+        assertEquals(10, peter.progress);
+    }
+    @Test
+    void posActRankPosRank() {
+        User peter = new User();
+
+        assertEquals(-8, peter.rank);
+        assertEquals(0, peter.progress);
+        assertEquals(10, peter.incProgress(1));
+        assertEquals(1, peter.rank);
+        assertEquals(10, peter.progress);
+        assertEquals(0, peter.incProgress(4));
+        assertEquals(2, peter.rank);
+        assertEquals(0, peter.progress);
+    }
+    @Test
+    void negActRankNegRankNarrow() {
+        User peter = new User();
+
+        assertEquals(-8, peter.rank);
+        assertEquals(0, peter.progress);
+        assertEquals(3, peter.incProgress(-8));
+        assertEquals(-8, peter.rank);
+        assertEquals(3, peter.progress);
+        assertEquals(63, peter.incProgress(-4));
+        assertEquals(-7, peter.rank);
+        assertEquals(63, peter.progress);
+        assertEquals(64, peter.incProgress(-8));
+        assertEquals(-7, peter.rank);
+        assertEquals(64, peter.progress);
+    }
+
+    @Test
+    void negActRankNegRankEdge() {
+        User peter = new User();
+
+        assertEquals(-8, peter.rank);
+        assertEquals(0, peter.progress);
+        assertEquals(3, peter.incProgress(-8));
+        assertEquals(-8, peter.rank);
+        assertEquals(3, peter.progress);
+        assertEquals(13, peter.incProgress(1));
+        assertEquals(1, peter.rank);
+        assertEquals(13, peter.progress);
+        assertEquals(13, peter.incProgress(-8));
+        assertEquals(1, peter.rank);
+        assertEquals(13, peter.progress);
+        RangeException thrown = Assertions.assertThrows(RangeException.class, () -> {
+            peter.incProgress(-9);
+        });
+        Assertions.assertEquals("out of range", thrown.getMessage());
+    }
+
+     @Test
+    void nobodyKnowsTheTroubleIveSeen(){
+        User peter = new User();
+         assertEquals(-8, peter.rank);
+         assertEquals(0, peter.progress);
+         assertEquals(90, peter.incProgress(-1));
+         assertEquals(-4, peter.rank);
+         assertEquals(90, peter.progress);
+         assertEquals(93, peter.incProgress(-4));
+         assertEquals(96, peter.incProgress(-4));
+         assertEquals(99, peter.incProgress(-4));
+         assertEquals(2, peter.incProgress(-4));
+         assertEquals(-3, peter.rank);
+         assertEquals(2, peter.progress);
+         assertEquals(62, peter.incProgress(1));
+         assertEquals(-2, peter.rank);
+         assertEquals(62, peter.progress);
+     }
 }
