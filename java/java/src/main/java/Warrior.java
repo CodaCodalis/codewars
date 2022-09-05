db6cccfe0af01864ba43d9d1660f7b420df4a323
CodaCodalis <lara.wuxus@gmail.com>
2022-09-05 15:38:39 +0200

    solved kata

--- java/java/src/main/java/Warrior.java
+++ java/java/src/main/java/Warrior.java
@@ -11,7 +11,7 @@ public class Warrior {
     }
 
     public int level() {
-        return level;
+        return this.level;
     }
 
     public int experience() {
@@ -38,41 +38,100 @@ public class Warrior {
             this.rank = rankArr[7];
         } else if (this.level < 90) {
             this.rank = rankArr[8];
-        } else if (this.level <= 100) {
+        } else if (this.level < 100) {
             this.rank = rankArr[9];
+        } else if (this.level == 100) {
+            this.rank = rankArr[10];
         }
         return this.rank;
     }
 
-    public ArrayList achievements() {
+    public int getRankInt(int level) {
+        int rank = 0;
+        if (level < 10)
+            rank = 1;
+        else if (level < 20) {
+            rank = 2;
+        } else if (level < 30) {
+            rank = 3;
+        } else if (level < 40) {
+            rank = 4;
+        } else if (level < 50) {
+            rank = 5;
+        } else if (level < 60) {
+            rank = 6;
+        } else if (level < 70) {
+            rank = 7;
+        } else if (level < 80) {
+            rank = 8;
+        } else if (level < 90) {
+            rank = 9;
+        } else if (level < 100) {
+            rank = 10;
+        } else if (level == 100) {
+            rank = 11;
+        }
+        return rank;
+    }
 
+    public ArrayList achievements() {
         return this.achievements;
     }
 
     public String training(String achievement, int exp, int level) {
-        this.experience += exp;
-        if (this.experience > 10000) {
-            this.experience = 10000;
+        if (this.level < level) {
+            return "Not strong enough";
+        } else {
+            this.experience += exp;
+            if (this.experience > 10000) {
+                this.experience = 10000;
+            }
+            this.level = this.experience / 100;
+            this.achievements.add(achievement);
+            return achievement;
         }
-        this.level = this.experience / 100;
-        this.achievements.add(achievement);
-        return achievement;
     }
 
     public String battle(int enemyLevel) {
-        if (enemyLevel < 1 || enemyLevel > 100) {
+        int rankWarrior = getRankInt(this.level);
+        int rankEnemy = getRankInt(enemyLevel);
+        int rankDiff = rankEnemy - rankWarrior;
+        int levelDiff = enemyLevel - this.level;
+        if (enemyLevel < 1 || enemyLevel > 100)
+        {
             return "Invalid level";
         }
-        if (this.level == enemyLevel) {
+        if (levelDiff == 0) // warrior: 1 <--> enemy: 1 (+10 xp)
+        {
             this.experience += 10;
-        } else if (this.level == enemyLevel + 1) {
-            this.experience += 5;
-        } else if (this.level >= enemyLevel - 2) {
-            // 0 experience
-        } else if (this.level > enemyLevel) {
-
+            this.level = this.experience / 100;
+            return "A good fight";
         }
-        return "A good fight";
+        else if (levelDiff == -1) // warrior: 5 <--> enemy: 4 (+5 xp)
+        {
+            this.experience += 5;
+            this.level = this.experience / 100;
+            return "A good fight";
+        }
+        else if (levelDiff <= -2) // warrior: 3 <--> enemy: 1 (0 xp)
+        {
+            return "Easy fight";
+        }
+        else if (rankDiff == 0) // warrior: 1 <--> enemy: 3 (+80 xp)
+        {
+            this.experience += (20 * ((enemyLevel - this.level) * (enemyLevel - this.level)));
+            this.level = this.experience / 100;
+            return "An intense fight";
+        }
+        else if (rankDiff == 1 && levelDiff < 5) // warrior: 1 <--> enemy: 3 (+80 xp)
+        {
+            this.experience += (20 * ((enemyLevel - this.level) * (enemyLevel - this.level)));
+            this.level = this.experience / 100;
+            return "An intense fight";
+        }
+        else
+        {
+            return "You've been defeated";
+        }
     }
 }
-
--- java/java/src/test/java/WarriorTest.java
+++ java/java/src/test/java/WarriorTest.java
@@ -7,11 +7,31 @@ import java.util.Arrays;
 
 class WarriorTest {
     @Test
-    void tomTest() {
+    void levelTest() {
         Warrior tom = new Warrior();
         assertEquals(1, tom.level());
         assertEquals(100, tom.experience());
         assertEquals("Pushover", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 900, 1));
+        assertEquals("Novice", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 1000, 2));
+        assertEquals("Fighter", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 1000, 3));
+        assertEquals("Warrior", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 1000, 4));
+        assertEquals("Veteran", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 1000, 5));
+        assertEquals("Sage", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 1000, 6));
+        assertEquals("Elite", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 1000, 7));
+        assertEquals("Conqueror", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 1000, 8));
+        assertEquals("Champion", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 1000, 9));
+        assertEquals("Master", tom.rank());
+        assertEquals("Defeated Chuck Norris", tom.training("Defeated Chuck Norris", 1000, 10));
+        assertEquals("Greatest", tom.rank());
     }
 
     @Test
@@ -29,5 +49,39 @@ class WarriorTest {
         assertEquals("A good fight", bruce_lee.battle(90));
         assertEquals(9105, bruce_lee.experience());
         assertEquals(Arrays.asList("Defeated Chuck Norris"), bruce_lee.achievements());
+        assertEquals(3, bruce_lee.getRankInt(29));
+        assertEquals(4, bruce_lee.getRankInt(39));
+        assertEquals(5, bruce_lee.getRankInt(49));
+        assertEquals(6, bruce_lee.getRankInt(59));
+        assertEquals(7, bruce_lee.getRankInt(69));
+        assertEquals(8, bruce_lee.getRankInt(79));
+        assertEquals(9, bruce_lee.getRankInt(89));
+        assertEquals(10, bruce_lee.getRankInt(99));
+        assertEquals(11, bruce_lee.getRankInt(100));
+    }
+
+    @Test
+    void defeatTest() {
+        Warrior jackie_chan = new Warrior();
+
+        assertEquals(1, jackie_chan.level());
+        assertEquals(100, jackie_chan.experience());
+        assertEquals("Pushover", jackie_chan.rank());
+        assertEquals(new ArrayList<String>(), jackie_chan.achievements());
+        assertEquals("Not strong enough", jackie_chan.training("Defeated Bruce Lee", 269, 6));
+        assertEquals(100, jackie_chan.experience());
+        assertEquals(1, jackie_chan.level());
+        assertEquals("Pushover", jackie_chan.rank());
+        assertEquals("Defeated Bruce Lee", jackie_chan.training("Defeated Bruce Lee", 790, 1));
+        assertEquals(890, jackie_chan.experience());
+        assertEquals(8, jackie_chan.level());
+        assertEquals("Pushover", jackie_chan.rank());
+        assertEquals("You've been defeated", jackie_chan.battle(19));
+        assertEquals("An intense fight", jackie_chan.battle(9));
+        assertEquals("An intense fight", jackie_chan.battle(13));
+        assertEquals("A good fight", jackie_chan.battle(12));
+        assertEquals("Easy fight", jackie_chan.battle(10));
+        assertEquals("Invalid level", jackie_chan.battle(110));
+        assertEquals("Defeated Bruce Lee", jackie_chan.training("Defeated Bruce Lee", 11000, 1));
     }
 }
