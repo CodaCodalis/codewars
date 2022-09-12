441a492e456ad1f2166fc383fe9f22d636e7b4d0
CodaCodalis <lara.wuxus@gmail.com>
2022-09-12 22:26:17 +0200

    solution approach in comments

--- python/next_bigger_number.py
+++ python/next_bigger_number.py
@@ -13,6 +13,15 @@ def next_bigger(n):
             res_next = "".join(str(x) for x in res)
             return int(res_next)
 
+    # if contains 9 -> new list "around_nine"
+    # 24178251|2965
+    # around nine = 2965
+    # next_bigger(2965) -> 5269
+    # append: 24178251|5269
+    # 1234567|890 -> 1234567908
+    # if contains 0 -> wtf
+    # 69800 -> 80069
+
     for i in range(0, len(res_rev) - 1):
         if res_rev[i] > res_rev[i + 1]:
             temp = res_rev[i]
--- python/test_next_bigger_number.py
+++ python/test_next_bigger_number.py
@@ -4,18 +4,21 @@ from next_bigger_number import next_bigger
 
 
 class Test(TestCase):
+    def test_big_next_bigger(self):
+        self.assertEqual(241782515269, next_bigger(241782512965))
+        self.assertEqual(1234567908, next_bigger(1234567890))
+        self.assertEqual(80069, next_bigger(69800))
+        self.assertEqual(67989, next_bigger(67899))
+        self.assertEqual(67908, next_bigger(67890))
+
     def test_next_bigger(self):
         self.assertEqual(351, next_bigger(315))
         self.assertEqual(21, next_bigger(12))
         self.assertEqual(2071, next_bigger(2017))
         self.assertEqual(441, next_bigger(414))
         self.assertEqual(414, next_bigger(144))
-        self.assertEqual(31, next_bigger(31))
+        self.assertEqual(-1, next_bigger(31))
         self.assertEqual(2170, next_bigger(2107))
         self.assertEqual(-1, next_bigger(9))
         self.assertEqual(-1, next_bigger(111))
         self.assertEqual(-1, next_bigger(531))
-
-    # def test_big_next_bigger(self):
-        # self.assertEqual(241782515269, next_bigger(241782512965))
-        # self.assertEqual(1234567908, next_bigger(1234567890))
