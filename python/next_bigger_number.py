c26e042e915834af33eb86667bfcd243a12aa5dc
CodaCodalis <lara.wuxus@gmail.com>
2022-09-13 10:56:31 +0200

    solution approach in comments

--- python/next_bigger_number.py
+++ python/next_bigger_number.py
@@ -1,38 +1,77 @@
 def next_bigger(n):
-    res = [int(x) for x in str(n)]
-    res_rev = res[::-1]
-    if len(res) == 1:
-        return -1
-    if len(res) == 2:
-        if res[0] > res[1]:
-            return n
+    n_arr = [int(x) for x in str(n)]
+    check_len_one_and_two(n_arr)
+    n_arr_next = []
+    for i in range(0, len(n_arr)):
+        if n_arr[i] == 9 or n_arr[i] == 0:
+            if n_arr[i] == 0:
+                n_arr_next = next_bigger_with_zero(n_arr, i)
+                break
+            elif n_arr[i] == 9:
+                n_arr_next = next_bigger_with_nine(n_arr, i)
+                break
         else:
-            temp = res[0]
-            res[0] = res[1]
-            res[1] = temp
-            res_next = "".join(str(x) for x in res)
-            return int(res_next)
+            n_arr_next = next_bigger_without_nine(n_arr)
 
-    # if contains 9 -> new list "around_nine"
-    # 24178251|2965
-    # around nine = 2965
-    # next_bigger(2965) -> 5269
-    # append: 24178251|5269
-    # 1234567|890 -> 1234567908
-    # if contains 0 -> wtf
-    # 69800 -> 80069
+    n_next = "".join(str(x) for x in n_arr_next)
+    if int(n_next) == n:
+        return -1
 
-    for i in range(0, len(res_rev) - 1):
-        if res_rev[i] > res_rev[i + 1]:
-            temp = res_rev[i]
-            res_rev[i] = res_rev[i + 1]
-            res_rev[i + 1] = temp
+    return int(n_next)
+
+
+def check_len_one_and_two(n):
+    if len(n) == 1:
+        return -1
+    if len(n) == 2:
+        if n[0] > n[1]:
+            return -1
+        else:
+            temp = n[0]
+            n[0] = n[1]
+            n[1] = temp
+            n_next = "".join(str(x) for x in n)
+            return int(n_next)
+
+
+def next_bigger_with_zero(n_arr, index):
+    # wtf
+    return -1
+
+
+def next_bigger_with_nine(n_arr, index):
+    after_nine = []
+    around_nine = []
+    for i in range(index + 1, len(n_arr)):
+        after_nine.append(n_arr[i])
+
+    after_nine_sorted = sorted(after_nine)
+    smallest_after_nine = after_nine_sorted[0]
+
+    for i in range(index - 1, len(n_arr)):
+        around_nine.append(n_arr[i])
+
+    n_arr_next = n_arr[:-(len(around_nine))]
+    n_arr_next.append(smallest_after_nine)
+
+    around_nine_sorted = sorted(around_nine)
+    around_nine_sorted.remove(smallest_after_nine)
+
+    for i in range(0, len(after_nine) + 1):
+        n_arr_next.append(around_nine_sorted[i])
+
+    return n_arr_next
+
+
+def next_bigger_without_nine(n_arr):
+    n_rev = n_arr[::-1]
+    for i in range(0, len(n_rev) - 1):
+        if n_rev[i] > n_rev[i + 1]:
+            temp = n_rev[i]
+            n_rev[i] = n_rev[i + 1]
+            n_rev[i + 1] = temp
             break
 
-    res = res_rev[::-1]
-    res_next = "".join(str(x) for x in res)
+    n_arr_next = n_rev[::-1]
 
-    if int(res_next) == n:
-        return -1
-
-    return int(res_next)
+    return n_arr_next
--- python/test_next_bigger_number.py
+++ python/test_next_bigger_number.py
@@ -5,11 +5,12 @@ from next_bigger_number import next_bigger
 
 class Test(TestCase):
     def test_big_next_bigger(self):
-        self.assertEqual(241782515269, next_bigger(241782512965))
+        self.assertEqual(67908, next_bigger(67890))
+        self.assertEqual(678900, next_bigger(678090))
         self.assertEqual(1234567908, next_bigger(1234567890))
+        self.assertEqual(241782515269, next_bigger(241782512965))
         self.assertEqual(80069, next_bigger(69800))
         self.assertEqual(67989, next_bigger(67899))
-        self.assertEqual(67908, next_bigger(67890))
 
     def test_next_bigger(self):
         self.assertEqual(351, next_bigger(315))
