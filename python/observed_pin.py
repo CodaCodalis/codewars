b0674083eafc3cd0c8dfdf45f4ff7d09d13f4d3c
CodaCodalis <lara.wuxus@gmail.com>
2022-10-19 15:29:11 +0200

    solve kata

--- python/observed_pin.py
+++ python/observed_pin.py
@@ -1,31 +1,21 @@
-from itertools import permutations
+from itertools import product
 
 
 def get_pins(observed):
-    if len(observed) == 1:
-        return possible_digits(observed)
-    else:
-        digits = observed.split()
-
-    for i in range(0, len(digits)-1):
-        if digits[0] != digits[i]:
-            break
-        else:
-            pass
-
-
-def possible_digits(digit):
-    digits = list()
     switch = {
-        '1': digits + ['1', '2', '4'],
-        '2': digits + ['1', '2', '3', '5'],
-        '3': digits + ['3', '2', '6'],
-        '4': digits + ['1', '4', '5', '7'],
-        '5': digits + ['2', '4', '5', '6', '8'],
-        '6': digits + ['3', '5', '6', '9'],
-        '7': digits + ['4', '7', '8'],
-        '8': digits + ['5', '7', '8', '9', '0'],
-        '9': digits + ['6', '8', '9'],
-        '0': digits + ['8', '0'],
-    }
-    return switch.get(digit, "Something went wrong.")
+        '1': ['1', '2', '4'],
+        '2': ['1', '2', '3', '5'],
+        '3': ['2', '3', '6'],
+        '4': ['1', '4', '5', '7'],
+        '5': ['2', '4', '5', '6', '8'],
+        '6': ['3', '5', '6', '9'],
+        '7': ['4', '7', '8'],
+        '8': ['5', '7', '8', '9', '0'],
+        '9': ['6', '8', '9'],
+        '0': ['8', '0']}
+
+    if len(observed) < 2:
+        pins = switch.get(observed)
+    else:
+        pins = list(map("".join, product(*[switch[i] for i in observed])))
+    return pins
--- python/test_observed_pin.py
+++ python/test_observed_pin.py
@@ -5,19 +5,8 @@ from observed_pin import get_pins
 class Test(TestCase):
     def test_get_pins(self):
         self.assertEqual(['5', '7', '8', '9', '0'], get_pins('8'))
-        self.assertEqual(["11", "22", "44", "12", "21", "14", "41", "24", "42"], get_pins('11'))
-        self.assertEqual(
+        self.assertEqual(sorted(["11", "22", "44", "12", "21", "14", "41", "24", "42"]), get_pins('11'))
+        self.assertEqual(sorted(
             ["339", "366", "399", "658", "636", "258", "268", "669", "668", "266", "369", "398", "256", "296", "259",
              "368", "638", "396", "238", "356", "659", "639", "666", "359", "336", "299", "338", "696", "269", "358",
-             "656", "698", "699", "298", "236", "239"], get_pins('369'))
-
-#  digits = {1,2,4}
-#  digits[0] + digits[0]    11
-#  digits[1] + digits[1]    22
-#  digits[2] + digits[2]    44
-#  digits[0] + digits[1]    12
-#  digits[1] + digits[0]    21
-#  digits[0] + digits[2]    14
-#  digits[2] + digits[0]    41
-#  digits[1] + digits[2]    24
-#  digits[2] + digits[1]    42
+             "656", "698", "699", "298", "236", "239"]), get_pins('369'))
